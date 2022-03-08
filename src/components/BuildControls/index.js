e49763d145cae4128f9d58e29534fe45f4b8b584
Batzaya <batzayabt@Batzaya-2.local>
2022-03-08 21:46:17 +0800

    Zahialga batalgaajuulah Modal tsonh

--- src/components/BuildControls/index.js
+++ src/components/BuildControls/index.js
@@ -1,31 +1,31 @@
 import React from "react";
 import BuildControl from "../BuildControl";
 import css from "./style.module.css";
-const BuildControls = (props) =>
-    <div className={css.BuildControls}>
-        <BuildControl 
-            addIngredient={props.addIngredient} 
-            removeIngredient={props.removeIngredient} 
-            desableIngredient={props.desableIngredient}
-            type="salad" 
-            ingredient="Салад"/>
-        <BuildControl 
-            addIngredient={props.addIngredient} 
-            removeIngredient={props.removeIngredient}
-            desableIngredient={props.desableIngredient}
-            type="bacon" 
-            ingredient="Гахайн мах"/>
-        <BuildControl 
-            addIngredient={props.addIngredient} 
-            removeIngredient={props.removeIngredient}
-            desableIngredient={props.desableIngredient}
-            type="cheese" 
-            ingredient="Бяслаг"/>
-        <BuildControl 
-            addIngredient={props.addIngredient}
-            removeIngredient={props.removeIngredient} 
-            desableIngredient={props.desableIngredient}
-            type="meat" 
-            ingredient="Үхрийн мах"/>
-    </div>
+const BuildControls = (props) =>{
+    const controls = {
+        salad: "Салад",
+        bacon: "Гахай мах",
+        cheese: "Бяслаг",
+        meat: "Үхрийн мах"
+    };
+    return (
+        <div className={css.BuildControls}>
+            <p>Бургерийн үнэ : <strong>{props.price}</strong> </p>
+            {
+                Object.keys(controls).map(el => (
+                    <BuildControl 
+                        key = {el}
+                        addIngredient={props.addIngredient} 
+                        removeIngredient={props.removeIngredient} 
+                        desableIngredient={props.desableIngredient}
+                        type={el}
+                        ingredient={controls[el]}
+                    />
+                ))
+            }
+            <button disabled={props.disabledOrder} className={css.OrderButton}>Захиалах</button>
+        </div>
+    );
+};
+
 export default BuildControls;
--- src/pages/BurgerPage/index.js
+++ src/pages/BurgerPage/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import Burger from "../../components/Burger";
 import BuildControls from "../../components/BuildControls";
-import { type } from "@testing-library/user-event/dist/type";
+import Modal from "../../components/General/Modal";
+//import { type } from "@testing-library/user-event/dist/type";
+const INGREDIENT_PRICES = {salad: 150, cheese: 250, bacon: 800, meat: 1500};
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -9,21 +11,22 @@ class BurgerBuilder extends Component {
             cheese: 0,
             bacon: 0,
             meat: 0
-        }
+        },
+        totalPrice: 0,
+        purchasing: false
     };
     addIngredient = (type) =>{
         const newAddIngredients = { ...this.state.ingredients };
         newAddIngredients[type]++;
-
-        this.setState({ingredients: newAddIngredients});
+        const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+        this.setState({purchasing: true, totalPrice: newPrice, ingredients: newAddIngredients});
     }
     removeIngredient = (type) =>{
         if(this.state.ingredients[type] > 0){
             const newRemoveIngredients = { ...this.state.ingredients };
             newRemoveIngredients[type]--;
-
-            this.setState({ingredients: newRemoveIngredients});
-
+            const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+            this.setState({purchasing: newPrice > 0, totalPrice: newPrice, ingredients: newRemoveIngredients});
         }
 
     }
@@ -31,12 +34,17 @@ class BurgerBuilder extends Component {
         const desableIngredient = {...this.state.ingredients};
         for(let key in desableIngredient){
             desableIngredient[key] = desableIngredient[key] <= 0;
-            console.log('=== ' + desableIngredient[key]);
         }
         return (
             <div>
+                <Modal>
+                   <h1>Та итгэлтэй байна уу...</h1> 
+                   <p>Захиалгын дэлгэрэнгүй</p>
+                </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
+                    disabledOrder={!this.state.purchasing}
+                    price={this.state.totalPrice}
                     addIngredient={this.addIngredient} 
                     removeIngredient={this.removeIngredient}
                     desableIngredient={desableIngredient}/>
