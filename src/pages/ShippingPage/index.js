946f24d9cea00065c20458c2e69bd26996ca6bef
Batzaya <batzaya@Batzaya.local>
2023-05-17 00:44:38 +0800

    packege version upgrade & shippingPage import change

--- src/pages/ShippingPage/index.js
+++ src/pages/ShippingPage/index.js
@@ -1,17 +1,17 @@
-import {React} from "react";
-import { Component } from "react/cjs/react.development";
+import React, {Component} from "react";
+//import { Component } from "react/cjs/react.development";
 import Burger from "../../components/Burger";
 import Button from "../../components/General/Button";
 import {Route} from "react-router-dom";
 import ContactData from "../../components/ContactData";
 import css from "./style.module.css";
 
- export class ShippingPage extends Component{
+export class ShippingPage extends Component{
 
     state = {
         price: 0,
         ingredients: null,
-     };
+    };
     //Render func  duudagdhaas umnu ajilna.
     componentWillMount(){
         const query = new URLSearchParams(this.props.location.search);
@@ -34,8 +34,8 @@ import css from "./style.module.css";
     showContactData = () => {
         this.props.history.replace('/ship/contact');
     }
-     render(){
-         return(
+    render(){
+        return(
             <div className={css.ShippingPage}> 
                 <p style={{fontSize: "24px"}}>
                     <strong>Таны захиалга</strong>
@@ -47,8 +47,8 @@ import css from "./style.module.css";
                 <Button 
                     clicked={this.cancelOrder} 
                     btnType="Danger" 
-                    text="ЗАХИАЛГА ЦУЦЛАХ"/
-                >
+                    text="ЗАХИАЛГА ЦУЦЛАХ"
+                />
                 <Button 
                     clicked={this.showContactData} 
                     btnType="Success" 
@@ -71,6 +71,6 @@ import css from "./style.module.css";
                     />
                 </Route>
             </div>
-         ); 
-     }
- }
+        ); 
+    }
+}
